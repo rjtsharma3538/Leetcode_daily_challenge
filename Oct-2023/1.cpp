9aafb7ad15ddc62310a8e446c297952b483d60dc
Rajat Sharma <rjtsharma3538@gmail.com>
2023-10-01 11:24:34 +0530

    Update 1.cpp

--- Oct-2023/1.cpp
+++ Oct-2023/1.cpp
@@ -1,4 +1,4 @@
-// this is leetcode daily challenge of 1 Sept 2023
+// this is leetcode daily challenge of 1 Oct 2023
 // problem link : https://leetcode.com/problems/reverse-words-in-a-string-iii/editorial/?envType=daily-question&envId=2023-10-01
 
 class Solution {
