a64fc605f241a21afe4fa84e488555f52cdb3cd4
Rajat Sharma <rjtsharma3538@gmail.com>
2023-10-01 11:15:05 +0530

    Create 1.cpp

---
+++ Oct-2023/1.cpp
@@ -0,0 +1,27 @@
+// this is leetcode daily challenge of 1 Sept 2023
+// problem link : https://leetcode.com/problems/reverse-words-in-a-string-iii/editorial/?envType=daily-question&envId=2023-10-01
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        string res="",temp="";
+        for(auto it:s)
+        {
+            if(it==' ')
+            {
+                reverse(temp.begin(),temp.end());
+                res+=temp;
+                res+=' ';
+                temp="";
+            }
+            else 
+                temp+=it;
+        }
+
+        reverse(temp.begin(),temp.end());
+        if(temp!=" ")
+            res+=temp;
+        
+        return res;
+    }
+};
